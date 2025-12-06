42cee5863f6c38f0c00eeed530ef67eab9edcb72
Manan-KK <1manankk@gmail.com>
2025-12-05 18:03:57 -0700

    docs

--- Demo/test.js
+++
@@ -1,9 +0,0 @@
-function printNameInConsole() {
-  //prompt on the browser to get user input
-  let name = prompt('Enter your name: ');
-  let age = prompt('Enter your age: ');
-  // prints name entered in the above step on the console in the browser.
-  console.log(name);
-  console.log(age);
-  
-}
